38df15eb5db80aed16c05c957d411b548fde1473
Kevin Dilts <kdilts13@gmail.com>
2016-09-15 19:05:28 -0600

    Added example of magic constants from the book.

--- index.php
+++ index.php
@@ -5,11 +5,16 @@
 	</head>
 	<body>
 		<?php
-			echo "Hello PHP";
-			echo "<h1>It is ". date("h:i:sa") ." on ".date("m/d/Y")."</h1>";
-			$myVar = "this is my variable";
-			echo "Double quotes: $myVar";
-			echo 'Single quotes: $myVar';
+
+		echo "Hello PHP";
+		echo "<h1>It is ". date("h:i:sa") ." on ".date("m/d/Y")."</h1>";
+
+		$myVar = "this is my variable";
+		echo "Double quotes: $myVar<br>";
+		echo 'Single quotes: $myVar<br><br>';
+
+		echo "This is line number " . __LINE__ . " of file " . __FILE__ . "<br><br>";
+
 		?>
 	</body>
 </html>
