c9800ca2d450a4a497b34abed59f5b86ba43fc71
Kevin Dilts <kdilts13@gmail.com>
2016-09-15 18:46:31 -0600

    Added two lines to show the difference between double and single quotes.

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
 	<body>
 		<?php
 			echo "Hello PHP";
-			echo "<h1>It is ". date("h:i:sa") ." on ".date("m/d/Y")."</h1>"
+			echo "<h1>It is ". date("h:i:sa") ." on ".date("m/d/Y")."</h1>";
+			$myVar = "this is my variable";
+			echo "Double quotes: $myVar";
+			echo 'Single quotes: $myVar';
 		?>
 	</body>
 </html>
